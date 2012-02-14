40f4b3bc596cd472815f2d3333e9eb5cb08a1d19
davidebbo <david.ebbo@microsoft.com>
2012-02-13 16:45:17 -0800

    Simple test to show the mono compiler at work

--- MonoCompilerDemo/Program.cs
+++ MonoCompilerDemo/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using Mono.CSharp;
 
 namespace MonoCompilerDemo
 {
+    public interface IFoo { string Bar(string s); }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var evaluator = new Evaluator(
+                new CompilerSettings(),
+                new Report(new ConsoleReportPrinter()));
+
+            evaluator.ReferenceAssembly(typeof(IFoo).Assembly);
+
+            evaluator.Compile(
+                @"
+    public class Foo : MonoCompilerDemo.IFoo
+    {
+        public string Bar(string s) { return s.ToUpper(); }
+    }");
+
+            for (; ; )
+            {
+                string line = Console.ReadLine();
+                if (line == null) break;
+
+                try
+                {
+                    Console.WriteLine(evaluator.Evaluate(line));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
